42a3f24544f5a2f42d261ebe3ff991a15dafcfcb
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-10-30 13:51:02 -0600

    Navbar styles finished

--- src/components/NavbarItem.vue
+++ src/components/NavbarItem.vue
@@ -1,16 +1,56 @@
 <template>
-  <nav>
-    <div>
-      <img alt='Imagen' />
-      <h1>Libreria</h1>
-    </div>
-    <ul>
-      <li>Inicio</li>
-      <li>Conocenos</li>
-      <li>Libros</li>
-      <li>Categorias</li>
-      <li>Contactanos</li>
-      <li>lupa icono</li>
-    </ul>
-  </nav>
+  <div class='navbar-container'>
+    <nav>
+      <div class='logo-container'>
+        <img src='../assets/library_logo_white.svg' />
+      </div>
+      <ul>
+        <li>Inicio</li>
+        <li>Conocenos</li>
+        <li>Libros</li>
+        <li>Categorias</li>
+        <li>Contactanos</li>
+        <li><i class='bi bi-search icon'></i></li>
+      </ul>
+    </nav>
+  </div>
 </template>
+<style scoped>
+
+.navbar-container {
+  width: 100%;
+  background-color: var(--navbar-background);
+}
+
+nav {
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 10px 0;
+  color: var(--navbar-text);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.logo-container {
+  max-width: 180px;
+  width: 100%;
+  cursor: pointer;
+}
+
+img {
+  width: 100%;
+}
+
+ul {
+  display: flex;
+  gap: 40px;
+}
+
+ul li {
+  list-style: none;
+  font-weight: 600;
+  cursor: pointer;
+}
+
+</style>
