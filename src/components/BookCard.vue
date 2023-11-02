323490dd65a0cbb22341bc3a66d22e4922985bb7
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-11-01 22:15:41 -0600

    Working on books card

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -1,11 +1,44 @@
 <template>
-  <div>
-    <div>
-      <img alt='imagen' />
+  <div class='bookcard-container'>
+    <div class='bookcard-container-image'>
+      <img src='https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=600' alt='imagen' />
     </div>
-    <div>
+    <div class='bookcard-container-text'>
       <h4>Lorem Ipsum</h4>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint aliquid quisquam illum voluptates pariatur eum vel eveniet ex officia recusandae amet omnis provident fugit nisi similique, unde quae voluptate quia.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint aliquid</p>
     </div>
   </div>
 </template>
+
+<style scoped>
+.bookcard-container {
+  height: 400px;
+}
+
+.bookcard-container-image {
+  height: 70%;
+}
+
+.bookcard-container-image img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.bookcard-container-text {
+  background-color: var(--bookcard-background-text);
+  height: 30%;
+  padding: 20px 10px;
+}
+
+.bookcard-container-text h4 {
+  height: 50%;
+  text-align: center;
+  font-size: clamp(16px, 1.1rem, 20px);
+}
+
+.bookcard-container-text p {
+  height: 50%;
+  text-align: left;
+}
+</style>
--- src/components/BooksShowcase.vue
+++ src/components/BooksShowcase.vue
@@ -1,14 +1,39 @@
 <script setup lang='ts'>
 import BookCard from './BookCard.vue';
 
-const example = Array(3).fill(0);
+const example = Array(8).fill(0);
 </script>
+
 <template>
-  <div>
+  <div class='container'>
     <h2>Libros</h2>
-    <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quisquam est aliquid provident ipsam, iure quo? Sapiente, ipsum fuga alias culpa soluta temporibus animi, fugit natus odio aspernatur, blanditiis nihil.</h5>
-    <div>
+    <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
+    <div class='container-cards'>
       <BookCard v-for='(demo, key) in example' :key='key' />
     </div>
   </div>
 </template>
+
+<style scoped>
+  .container {
+    max-width: var(--main-block-item-max-width);
+    margin: 0 auto;
+    margin-top: 100px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .container h2 {
+    font-size: clamp(32px, 3rem, 64px);
+  }
+
+  .container-cards {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: minmax(100px, auto);
+    gap: 15px;
+    margin-top: 50px;
+  }
+</style>
